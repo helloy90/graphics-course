e26d99ea175aa779ee5d30696e847366fac682ed
helloy90 <98900889+helloy90@users.noreply.github.com>
2024-10-25 15:00:26 +0300

    begin bakery

--- tasks/model_bakery/baker/main.cpp
+++ tasks/model_bakery/baker/main.cpp
@@ -1,4 +1,13 @@
-int main()
+#include "etna/Assert.hpp"
+
+#include <string>
+
+int main(int argc, char* argv[])
 {
+  if (argc != 1) {
+    ETNA_PANIC("Arguments amount is not correct! Expected 1 argument - path to .gltf file.");
+  }
+
+  auto path = std::string(argv[1]);
   return 0;
 }
